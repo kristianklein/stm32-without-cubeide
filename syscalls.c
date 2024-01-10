b798d324c8e641fff432e75908528b2da909d1e7
Kristian Klein-Wengel <kkw@kleinembedded.com>
2024-01-10 14:06:30 +0100

    Use correct linker symbol for end of bss section

--- syscalls.c
+++ syscalls.c
@@ -69,12 +69,12 @@ int _read(int file, char *ptr, int len) {
 register char * stack_ptr asm("sp");
 
 caddr_t _sbrk(int incr) {
-  extern char _end;		/* Defined by the linker */
+  extern char __bss_end__;		/* Defined by the linker */
   static char *heap_end;
   char *prev_heap_end;
  
   if (heap_end == 0) {
-    heap_end = &_end;
+    heap_end = &__bss_end__;
   }
   prev_heap_end = heap_end;
   if (heap_end + incr > stack_ptr) {
